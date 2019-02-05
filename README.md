854d7eb5efbefe6669420ff0c2aa487520ffe134
Emad Ibrahim <eibrahim@gmail.com>
2019-02-05 18:09:18 -0500

    added dropdown requests and oauth

--- README.md
+++ README.md
@@ -1,8 +1,6 @@
 # Hyperslack
 
-Welcome to your new gem! In this directory, you'll find the files you need to be able to package up your Ruby library into a gem. Put your Ruby code in the file `lib/hyperslack`. To experiment with that code, run `bin/console` for an interactive prompt.
-
-TODO: Delete this and the text above, and describe your gem
+A gem to make it a bit easier to work with slack API inside web apps.  We use it in our slack integration at [Hyperlogs](https://www.hyperlogs.com)
 
 ## Installation
 
--- lib/hyperslack.rb
+++ lib/hyperslack.rb
@@ -1,6 +1,90 @@
 require "hyperslack/version"
+require 'net/http'
+require 'json'
 
 module Hyperslack
   class Error < StandardError; end
-  # Your code goes here...
+
+  class Slack
+    def oauth(client_id, client_secret, code, redirect_uri=nil)
+      uri = URI("https://slack.com/api/oauth.access")
+      body = {
+        client_id: client_id,
+        client_secret: client_secret,
+        code: code
+      }
+      body[:redirect_uri] = redirect_uri unless redirect_uri.nil?
+
+      uri.query = URI.encode_www_form(body)
+
+      http = Net::HTTP.new(uri.host, uri.port)
+      http.use_ssl = true
+      http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+      req = Net::HTTP::Post.new(uri, {'Content-Type' =>'application/x-www-form-urlencoded'})
+
+      res = http.request(req)
+      return JSON.parse(res.body)
+    end
+
+    def send_message(url, message)
+      uri = URI(url)
+      http = Net::HTTP.new(uri.host, uri.port)
+      http.use_ssl = true
+      http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+      req = Net::HTTP::Post.new(uri, {'Content-Type' =>'application/json'})
+
+      body = {
+        "text": message
+      }
+      req.body = body.to_json
+      res = http.request(req)
+    end
+
+    def request_drop_down(url, prompt, callback_id, action_name, dropdown_options,
+                          is_grouped=false,
+                          color="0277BD")
+
+      body = create_drop_down prompt, callback_id, action_name, dropdown_options,
+        is_grouped,
+        color
+      return make_request url, body
+    end
+
+    def make_request (url, body)
+      uri = URI(url)
+      http = Net::HTTP.new(uri.host, uri.port)
+      http.use_ssl = true
+      http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+      headers = {'Content-Type' => 'application/json'}
+  end
+
+
+  def create_drop_down(prompt, callback_id, action_name, dropdown_options,
+                       is_grouped=false,
+                       color="0277BD")
+    body = {
+      "attachments":  [
+        {
+          "text": prompt,
+          "attachment_type": "default",
+          "callback_id": "#{callback_id}",
+          "actions": [
+            {
+              "name": action_name,
+              "type": "select",
+            }
+          ]
+        }
+      ]
+    }
+
+    body[:attachments][0][:color] = color unless color.nil?
+    body[:attachments][0][:actions][0][:options] = dropdown_options unless is_grouped
+    body[:attachments][0][:actions][0][:option_groups] = dropdown_options if is_grouped
+
+    return body
+  end
+
+end
+
 end
